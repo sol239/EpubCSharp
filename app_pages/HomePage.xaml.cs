7359634683b6dca8f94c735a28b75945231aefdc
david_valek17 <david.valek17@gmail.com>
2024-08-16 15:48:25 +0200

    Add more debugs

--- app_pages/HomePage.xaml.cs
+++ app_pages/HomePage.xaml.cs
@@ -2,22 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using EpubCSharp.code;
-using Microsoft.UI.Text;
-using Microsoft.UI;
 using System.Text.Json;
+using EpubCSharp.code;
+using Microsoft.UI;
+using Microsoft.UI.Text;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
